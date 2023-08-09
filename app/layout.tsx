6c1e70884d54c667ed41ee8d6dc316c5de213df3
Dima <dimashikalov86@mail.ru>
2023-08-09 12:36:59 +0300

    fix rating

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,28 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter, Noto_Sans } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Noto_Sans } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Проект на Next',
-  description: 'Learning project',
+  title: "Проект на Next",
+  description: "Learning project",
 };
 
 const noto_sans = Noto_Sans({
-  weight: ['300', '400', '500', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap'
+  weight: ["300", "400", "500", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={noto_sans.className}>
-      
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,46 @@
-import { Metadata } from 'next';
-import { Button, Htag , P, Tag} from './components';
+import { Metadata } from "next";
+import { Button, Htag, P, Rating, Tag } from "./components";
 
 export const metadata: Metadata = {
-  title: 'Home page',
-  description: 'Some text',
+  title: "Home page",
+  description: "Some text",
 };
 
 export default function Home() {
   return (
     <>
-      <Htag tag='h2'>Hello Dimas</Htag>
-      <Button appearance='primary' arrow='right' >
+      <Htag tag="h2">Hello Dimas</Htag>
+      <Button appearance="primary" arrow="right">
         CLick me
       </Button>
-         
-      <Button appearance='ghost' arrow='down'>CLick me</Button>
-      <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cupiditate.</P>
-      <P size='s'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cupiditate.</P>
-      <P size="l">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cupiditate.</P>
-      <P >Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cupiditate.</P>
-      <Tag size='s'>Small</Tag>
-      <Tag size='m' color="red">mediem</Tag>
-      <Tag size='m' color="primary">mediem</Tag>
+
+      <Button appearance="ghost" arrow="down">
+        CLick me
+      </Button>
+      <P>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+        cupiditate.
+      </P>
+      <P size="s">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+        cupiditate.
+      </P>
+      <P size="l">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+        cupiditate.
+      </P>
+      <P>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+        cupiditate.
+      </P>
+      <Tag size="s">Small</Tag>
+      <Tag size="m" color="red">
+        mediem
+      </Tag>
+      <Tag size="m" color="primary">
+        mediem
+      </Tag>
+      <Rating isEditable />
     </>
   );
 }
